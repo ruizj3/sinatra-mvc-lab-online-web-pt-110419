abfd0784181c8e6ee29a5954497471d1988b885a
Joseph Ruiz <ruizj3@uw.edu>
2020-02-27 02:23:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
   post '/piglatinize' do
     pl = PigLatinizer.new
     @piglatin = pl.piglatinize(params[:user_phrase])
-    erb :results
+    erb :user_input
   end
 
 end
